9a42fb14203a67bd34f050e2b58a4f844b62ceb8
Alex <tulupov.alex@gmail.com>
2016-04-02 17:34:21 +0300

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "3245897");
-define("DB_DATABASE", "schedule");
-
-define("GOOGLE_API_KEY", "AIzaSyBgsd0F5Vl_ztwQum-aNTUdjtpGH3WhSU4");
